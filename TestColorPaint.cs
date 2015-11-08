2c0652f18cf59155e7aaad658591e6fdad70a558
Botmasher <languagejosh@hotmail.com>
2015-11-08 12:56:29 -0500

    Configure scripts to pick colors from one texture and paint them on a newly generated texture on a separate object

---
+++ TestColorPaint.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestColorPaint : MonoBehaviour {
+
+	// brush variables
+	public static Color brushColor;
+	public int size;
+	private int brushSize;
+
+	// for changing specific clicked pixels
+	Ray ray;
+	RaycastHit hit;
+	Texture2D tex;
+
+
+	void Start () {
+		// initial brush state
+		brushColor = Color.black;
+		brushSize = (int)(size*0.5f);
+
+		// generate texture for this object
+		tex = new Texture2D(600, 800);
+		GetComponent<Renderer>().material.mainTexture = tex;
+	}
+
+
+	void Update () {
+	
+		// raycast check that this gameobject was hit
+		if (Input.GetMouseButton(0)) {
+			ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			Physics.Raycast(ray, out hit);
+
+			if (hit.collider != null && hit.collider.gameObject == this.gameObject) {
+
+				/**
+				 * iterate over a box of rows and columns, coloring each pixel in that square
+				 * 	- iterator starts neg and ends at half of public size var to keep brush edges equidistant from cursor
+				 *  - texture.Apply() stores the changes, but is expensive
+				 */
+				for (int i=(int)(-brushSize); i<=brushSize; i++) {
+					for (int j=(int)(-brushSize); j<=brushSize; j++) {
+						// use the x/y of the exact raycast pixel hit as reference; set it and surrounding pixels to color
+						tex.SetPixel ((int)(tex.width*hit.textureCoord.x+i), (int)(tex.height*hit.textureCoord.y+j), brushColor);
+					}
+				}
+				tex.Apply();
+			}
+
+		}
+
+	}
+
+}
---
+++ TestColorPicker.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestColorPicker : MonoBehaviour {
+
+	// for pixel color data from hit object
+	RaycastHit hit;
+	Ray ray;
+	Texture2D tex;
+	Color pixelColor;
+
+	// gameobject to change
+	public GameObject target;
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			// cast ray looking for collider with texture
+			ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			Physics.Raycast(ray, out hit);
+			// /!\ make sure collider is mesh collider otherwise hit.textureCoord returns Vector2.zero
+			if (hit.collider.name == this.name) {
+				//Debug.Log (hit.collider.name);
+				tex = this.GetComponent<MeshRenderer> ().material.mainTexture as Texture2D;
+				//Debug.Log (hit.textureCoord);
+
+				pixelColor = tex.GetPixel((int)(hit.textureCoord.x*tex.width), (int)(hit.textureCoord.y*tex.height));
+
+				// change color of pixel painter in color paint script
+				TestColorPaint.brushColor = pixelColor;
+
+				// change color of target object using tex data
+				target.GetComponent<MeshRenderer> ().material.color = pixelColor;
+			}
+		}
+	}
+}
